73530d9a8f325f2cea7371b9a15213c7247b58e9
Markus Eble <markus.eble@online.de>
2022-04-16 17:05:06 +0200

    state machine string literal main program

---
+++ StateMachineTableStringLiteralMain.java
@@ -0,0 +1,16 @@
+
+import java.io.OutputStreamWriter;
+
+public class StateMachineTableStringLiteralMain {
+
+	public static void main(String[] args) throws Exception {
+		compiler.StateMachine stringMachine = new StateMachineTableStringLiteral();
+		OutputStreamWriter outStream = new OutputStreamWriter(System.out, "UTF-8");
+		stringMachine.process("\"ABB\"", outStream);
+
+        System.out.println();
+        System.out.println(stringMachine.asDot());
+		System.out.println();
+	}
+
+}
