d415f435d6b72cf20e1683bd7b92d2de5e345080
Markus Eble <markus.eble@online.de>
2022-04-16 17:14:24 +0200

    state machine decimals main program

---
+++ StateMachineDecimalsMain.java
@@ -0,0 +1,16 @@
+
+import java.io.OutputStreamWriter;
+
+public class StateMachineDecimalsMain {
+
+	public static void main(String[] args) throws Exception {
+		compiler.StateMachine decimalMachine = new StateMachineDecimals();
+		OutputStreamWriter outStream = new OutputStreamWriter(System.out, "UTF-8");
+		decimalMachine.process("-417.18", outStream);
+
+        System.out.println();
+        System.out.println(decimalMachine.asDot());
+		System.out.println();
+	}
+
+}
