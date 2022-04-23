1f7303a3c3439d4358e2765b088d32816f592b41
TheRaynex <rafael.luedtke@web.de>
2022-04-23 18:40:58 +0200

    renamed files

--- StateMachineCharMain.java
+++ StateMachineCharMain.java
@@ -3,14 +3,16 @@ import java.io.OutputStreamWriter;
 
 public class StateMachineCharMain {
 
-	public static void main(String[] args) throws Exception {
-		compiler.StateMachine stateMachineChar = new StateMachineChar();
-		OutputStreamWriter outStream = new OutputStreamWriter(System.out, "UTF-8");
-		stateMachineChar.process("'\\'", outStream);
+    public static void main(String[] args) throws Exception {
+        compiler.StateMachine stateMachineChar = new StateMachineChar();
+        OutputStreamWriter outStream = new OutputStreamWriter(System.out, "UTF-8");
+        stateMachineChar.process("'\\'", outStream);
 
         System.out.println();
         System.out.println(stateMachineChar.asDot());
-		System.out.println();
-	}
+        System.out.println();
+    }
 
 }
+
+
