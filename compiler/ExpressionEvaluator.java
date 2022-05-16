971a1399f1d80164470e4c2500ebc51b1b7e9843
nkilders <noah.kilders@gmail.com>
2022-05-16 11:49:41 +0200

    Revert "add ? operator"
    This reverts commit 018b68697096464981bbaa5030b7fe791e229aad.

--- compiler/ExpressionEvaluator.java
+++ compiler/ExpressionEvaluator.java
@@ -1,71 +1,67 @@
 package compiler;
 
 public class ExpressionEvaluator {
-	private Lexer m_lexer;
+    private Lexer m_lexer;
+    
+    public ExpressionEvaluator(Lexer lexer) {
+        m_lexer = lexer;
+    }
+    
+    public int eval(String val) throws Exception {
+        m_lexer.init(val);
+        return getQuestionMarkExpr();
+    }
+    
+    int getParantheseExpr() throws Exception {
+        Token curToken = m_lexer.lookAhead();
+        m_lexer.expect(Token.Type.INTEGER);
+        return Integer.valueOf(curToken.m_value);
+    }
+    
+    int getUnaryExpr() throws Exception {
+        return getParantheseExpr();
+    }
+    
+    int getMulDivExpr() throws Exception {
+        return getUnaryExpr();
+    }
 
-	public ExpressionEvaluator(Lexer lexer) {
-		m_lexer = lexer;
-	}
+    // plusMinusExpr : mulDivExpr ((PLUS|MINUS) mulDivExpr)*
+    int getPlusMinusExpr() throws Exception {
+        int result = getMulDivExpr();
+        while(
+            m_lexer.lookAhead().m_type == Token.Type.PLUS ||
+            m_lexer.lookAhead().m_type == Token.Type.MINUS)
+        {
+            Token nextToken = m_lexer.lookAhead();
+            if (nextToken.m_type == Token.Type.PLUS) {
+                m_lexer.expect(Token.Type.PLUS);
+                result += getMulDivExpr();
+            } else {
+                m_lexer.expect(Token.Type.MINUS);
+                result -= getMulDivExpr();
+            }
+        }
+        return result;
+    }
 
-	public int eval(String val) throws Exception {
-		m_lexer.init(val);
-		return getQuestionMarkExpr();
-	}
+    int getBitAndOrExpr() throws Exception {
+        return getPlusMinusExpr();
+    }
 
-	int getParantheseExpr() throws Exception {
-		Token curToken = m_lexer.lookAhead();
-		m_lexer.expect(Token.Type.INTEGER);
-		return Integer.valueOf(curToken.m_value);
-	}
+    int getShiftExpr() throws Exception {
+        return getBitAndOrExpr();
+    }
 
-	int getUnaryExpr() throws Exception {
-		return getParantheseExpr();
-	}
+    int getCompareExpr() throws Exception {
+        return getShiftExpr();
+    }
 
-	int getMulDivExpr() throws Exception {
-		return getUnaryExpr();
-	}
+    int getAndOrExpr() throws Exception {
+        return getCompareExpr();
+    }
 
-	// plusMinusExpr : mulDivExpr ((PLUS|MINUS) mulDivExpr)*
-	int getPlusMinusExpr() throws Exception {
-		int result = getMulDivExpr();
-		while (m_lexer.lookAhead().m_type == Token.Type.PLUS || m_lexer.lookAhead().m_type == Token.Type.MINUS) {
-			Token nextToken = m_lexer.lookAhead();
-			if (nextToken.m_type == Token.Type.PLUS) {
-				m_lexer.expect(Token.Type.PLUS);
-				result += getMulDivExpr();
-			} else {
-				m_lexer.expect(Token.Type.MINUS);
-				result -= getMulDivExpr();
-			}
-		}
-		return result;
-	}
-
-	int getBitAndOrExpr() throws Exception {
-		return getPlusMinusExpr();
-	}
-
-	int getShiftExpr() throws Exception {
-		return getBitAndOrExpr();
-	}
-
-	int getCompareExpr() throws Exception {
-		return getShiftExpr();
-	}
-
-	int getAndOrExpr() throws Exception {
-		return getCompareExpr();
-	}
-
-	// questionmark: number questionmark number doublecolon number
-	int getQuestionMarkExpr() throws Exception {
-		int toResolve = getAndOrExpr();
-		m_lexer.expect(Token.Type.QUESTIONMARK);
-		int trueNum = getAndOrExpr();
-		m_lexer.expect(Token.Type.DOUBLECOLON);
-		int falseNum = getAndOrExpr();
-
-		return toResolve == 1 ? trueNum : falseNum;
-	}
+    int getQuestionMarkExpr() throws Exception {
+        return getAndOrExpr();
+    }
 }
